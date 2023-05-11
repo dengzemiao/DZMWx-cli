26f0087fa73d55c9ede26e7e4015967c18eb7cb8
dengzemiao <dengzemiaovip@163.com>
2023-05-11 11:46:57 +0800

    .

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ async function run() {
 	})
 	// 上传结果
 	console.log('=============================='.bgGreen);
-	console.log(`APPID：${config.appid}\n项目名：${config.projectname}\n版本号：${version}\n打包环境：${process.env.NODE_ENV}\n上传结果：成功`.green);
+	console.log(`APPID：${config.appid}\n项目名：${config.projectname}\n版本号：${version}\n打包环境：${process.env.NODE_ENV}\n上传结果：成功\n上传时间：${dayjs().format('YYYY-MM-DD HH:mm:ss')}`.green);
 	console.log('测试、发包直接前往：小程序后台管理【版本管理】中扫码体验、测试、发包！'.yellow);
 	// console.log(uploadResult);
 	console.log('=============================='.bgGreen);
